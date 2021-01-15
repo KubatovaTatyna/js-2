df67d2cb6f8ff530d92460038f980fe63659e682
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-01-15 21:28:22 +0600

    Сalculation prompt

--- scripts.js
+++ scripts.js
@@ -23,13 +23,34 @@ title = "The Ship of Brides";
 author = "Jojo Moyes";
 
 let artist = "Selena Gomez";
+let movie = "Money Hest";
 
-console.log(fifth);
-console.log( capital + " is the capital of " + country);
-console.log( subject + " is measured in " + unit)
-console.log("My fovourite book is " + title + " by " + author)
-console.log("My favourite song is \"Yesterday\" by Beatles")
-console.log("My favourite song is 'Yesterday' by Beatles")
-console.log('I\'m the best')
-console.log("I'm the fan of \""+ artist + "\"!")
-console.log(`I'm the fan of "`+ artist +`"!`)
+// console.log(fifth);
+// console.log( capital + " is the capital of " + country);
+// console.log( subject + " is measured in " + unit);
+// console.log("My fovourite book is " + title + " by " + author);
+// console.log("My favourite song is \"Yesterday\" by Beatles");
+// console.log("My favourite song is 'Yesterday' by Beatles");
+// console.log('I\'m the best');
+// console.log("I'm the fan of \""+ artist + "\"!");
+// console.log(`I'm the fan of "`+ artist +`"!`);
+// console.log(`My favourite movie is "`+ movie + `"`);
+// console.log(`My favourite movie is "${movie}"`);
+
+// let firstName = prompt('What is your name?');
+
+// console.log(`Your name is ${firstName}`);
+
+// let a = +prompt("What is A?") ;
+// let b = +prompt("What is B?");
+// let sum = a + b;
+
+// console.log(`The sum of two numbrs is equal to ${sum}`);
+
+let c = +prompt("What is C?")
+let d = +prompt("What is D?")
+let e = c * d;
+console.log(`C multiplied by D is equal to ${e}`)
+
+
+
