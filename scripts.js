26d852565a83d19a1330b8194dcd627924cfdb31
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-01-15 14:54:48 +0600

    Perforemed others examples

--- scripts.js
+++ scripts.js
@@ -4,4 +4,25 @@ let third = second;
 let fourth = third;
 let fifth = fourth;
 
+let capital = "New-Delhi";
+let country = "India";
+
+let unit = "Distance";
+let subject = "meters";
+
+unit = "pounds";
+subject = "Weight";
+
+subject = "Temperature";
+unit = "degrees"; 
+
+let title = "Pride & Prejudice"
+let author = "Jane Austen!"
+
+title = "The Ship of Brides"
+author = "Jojo Moyes"
+
 console.log(fifth);
+console.log( capital + " is the capital of " + country);
+console.log( subject + " is measured in " + unit)
+console.log("My fovourite book is " + title + " by " + author)
